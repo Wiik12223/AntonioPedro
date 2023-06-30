4225e580d9790ef968a4209b7d78743285ecb37d
Antonio Pedro <60760632+Wiik12223@users.noreply.github.com>
2023-06-30 01:57:25 -0300

    ta dando bom

--- lojabicicleta/Bicicleta.java
+++ lojabicicleta/Bicicleta.java
@@ -1,11 +1,26 @@
 class Bicicleta {
     private String marca;
-    private int aro, preco;
+    private int aro;
+    private int preco;
+    private int quantidadeEstoque;
 
-    public Bicicleta(String marca, int aro, int preco) {
+    public Bicicleta(String marca, int aro, int preco, int quantidadeEstoque) {
         this.marca = marca;
         this.aro = aro;
         this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void setAro(int aro) {
+        this.aro = aro;
+    }
+
+    public void setPreco(int preco){
+        this.preco = preco;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
     }
 
     public String getMarca() {
@@ -18,5 +33,25 @@ class Bicicleta {
 
     public int getPreco() {
         return preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void reduzirEstoque(int quantidade) {
+        quantidadeEstoque -= quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return  "Marca: " + marca +
+                ", Aro: " + aro +
+                ", Preço: " + preco +
+                ", Quantidade em estoque: " + quantidadeEstoque;
     }
 }
--- lojabicicleta/LojaBicicletas.java
+++ lojabicicleta/LojaBicicletas.java
@@ -1,8 +1,10 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 class LojaBicicletas {
     private List<Bicicleta> bicicletas;
@@ -13,27 +15,97 @@ class LojaBicicletas {
 
     public void adicionarBicicleta(Bicicleta bicicleta) {
         bicicletas.add(bicicleta);
+        mostrarBicicletas();
     }
 
+    public void atualizarBicicleta(Bicicleta bicicleta) {
+        // Encontre a bicicleta existente com base na marca, por exemplo
+        for (int i = 0; i < bicicletas.size(); i++) {
+            Bicicleta b = bicicletas.get(i);
+            if (b.getMarca().equals(bicicleta.getMarca())) {
+                bicicletas.set(i, bicicleta);
+                break;
+            }
+        }
+    }
+
+     public Bicicleta buscarBicicleta(String marca) {
+        for (Bicicleta bicicleta : bicicletas) {
+            if (bicicleta.getMarca().equals(marca)) {
+                return bicicleta;
+            }
+        }
+        return null;
+    }
+
+    public void removerBicicleta(Bicicleta bicicleta) {
+        bicicletas.remove(bicicleta);
+    }
     public void carregarBicicletasDeArquivo(String diretorioAtual, String nomeArquivo) {
-        String caminhoArquivo = diretorioAtual + "/" + nomeArquivo;
+        String caminhoArquivo = diretorioAtual + File.separator + nomeArquivo;
+        File arquivo = new File(caminhoArquivo);
+        if (!arquivo.exists()) {
+            // Lógica para lidar com o arquivo inexistente
+            return;
+        }
+        
         try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
             String linha;
             while ((linha = br.readLine()) != null) {
                 String[] partes = linha.split(",");
-                if (partes.length == 3) {
+                if (partes.length == 4) {
                     String marca = partes[0].trim();
-                    int aro = Integer.parseInt(partes[1].trim());
-                    int preco = Integer.parseInt(partes[2].trim());
-                    Bicicleta bicicleta = new Bicicleta(marca, aro, preco);
-                    adicionarBicicleta(bicicleta);
-                }
+                    int aro;
+                    int preco;
+                    int quantidadeEstoque;
+                    try {
+                        aro = Integer.parseInt(partes[1].trim());
+                        preco = Integer.parseInt(partes[2].trim());
+                        quantidadeEstoque = Integer.parseInt(partes[3].trim());
+                    } catch (NumberFormatException e) {
+                        // Lógica para lidar com a conversão de número inválido
+                        continue;
+                    }
+                Bicicleta bicicleta = new Bicicleta(marca, aro, preco, quantidadeEstoque);
+                adicionarBicicleta(bicicleta);
+            }
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+    public List<Bicicleta> getBicicletas() {
+        return bicicletas;
+    }
+
+    public void salvarBicicletas(String nomeArquivo) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(nomeArquivo))) {
+            for (Bicicleta bicicleta : bicicletas) {
+                writer.println(bicicleta.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public List<Bicicleta> getBicicletas() {
-            return bicicletas;
+
+    public void mostrarBicicletas() {
+        JFrame frame = new JFrame("Bicicletas");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        for (Bicicleta bicicleta : bicicletas) {
+            textArea.append(bicicleta.toString() + "\n");
         }
+
+        frame.add(scrollPane);
+        frame.setVisible(true);
+    }
+    
+        
 }
